282a7b4c181e540163c7ef3c524fae8e18e74470
Thiago Ferolla <thiagofunascimento@gmail.com>
2023-02-06 15:11:04 -0300

    feat: added initial font loading

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
+import useFonts from "./src/hooks/useFonts";
 import MainNavigator from "./src/navigation/MainNavigator";
 
 export default function App() {
+  const fontsLoaded = useFonts();
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <MainNavigator />;
 }
---
+++ src/hooks/useFonts.tsx
@@ -0,0 +1,26 @@
+import { useFonts as _useFonts } from "expo-font";
+import {
+  Sora_100Thin,
+  Sora_200ExtraLight,
+  Sora_300Light,
+  Sora_400Regular,
+  Sora_500Medium,
+  Sora_600SemiBold,
+  Sora_700Bold,
+  Sora_800ExtraBold,
+} from "@expo-google-fonts/sora";
+
+export default function useFonts(): boolean {
+  let [fontsLoaded] = _useFonts({
+    Sora_100Thin,
+    Sora_200ExtraLight,
+    Sora_300Light,
+    Sora_400Regular,
+    Sora_500Medium,
+    Sora_600SemiBold,
+    Sora_700Bold,
+    Sora_800ExtraBold,
+  });
+
+  return fontsLoaded;
+}
